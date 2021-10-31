f8d0b36f6ad9a4baff9074b1ba7d6f48f28cf80c
Van Nguyen <68780111+tankadev@users.noreply.github.com>
2021-10-31 11:14:38 +0700

    add category

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,18 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const header = document.getElementById('header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
+
 /*==================== HOME SWIPER ====================*/
 let homeSwiper = new Swiper('.home-swiper', {
     spaceBetween: 30,
