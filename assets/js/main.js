ad36503da4a4d4665d4ea9f9e7fe814e2a626674
Van Nguyen <68780111+tankadev@users.noreply.github.com>
2021-11-01 21:35:59 +0700

    add discount, new, newsletter, footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,31 @@ let homeSwiper = new Swiper('.home-swiper', {
         clickable: true
     },
 });
+
+/*==================== NEW SWIPER ====================*/
+let newSwiper = new Swiper('.new-swiper', {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    loop: 'true'
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
