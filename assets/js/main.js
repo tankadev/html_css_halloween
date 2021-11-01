d67c2770c2aa19defb328b1eaede04d7c7cac970
Van Nguyen <68780111+tankadev@users.noreply.github.com>
2021-11-01 22:55:51 +0700

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,29 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 460) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+});
+
+sr.reveal('.home-swiper, .new-swiper, .newsletter__container');
+sr.reveal('.category__data, .trick__content, .footer__content', { interval: 100 });
+sr.reveal('.about__data, .discount__img', { origin: 'left' });
+sr.reveal('.about__img, .discount__data', { origin: 'right' });
